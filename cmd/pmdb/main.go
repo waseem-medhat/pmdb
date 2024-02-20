8d0c140573aca8090819d830021dfd76adfdee4b
wipdev-tech <wipdev.tech@gmail.com>
2024-02-20 18:47:46 +0200

    Successful DB connection + first query

--- cmd/pmdb/main.go
+++ cmd/pmdb/main.go
@@ -1,39 +1,60 @@
+// package main is the entry point of the PMDb app
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"text/template"
 
-	"github.com/joho/godotenv"
+	_ "github.com/tursodatabase/libsql-client-go/libsql"
+	"github.com/wipdev-tech/pmdb/internal/database"
 )
 
-func main() {
-	fmt.Println("PMDb Server Let's Go! ")
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-	http.HandleFunc("/", handleHome)
+type service struct {
+	db *database.Queries
+}
 
-	err := godotenv.Load()
+func main() {
+	dbURL, dbToken, err := getDBEnv()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	connURL := fmt.Sprintf("%s?authToken=%s", dbURL, dbToken)
+	db, err := sql.Open("libsql", connURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := service{db: database.New(db)}
+
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.HandleFunc("/", s.handleHome)
+
+	fmt.Println("PMDb server let's Go! ")
 	if os.Getenv("ENV") == "dev" {
 		fmt.Println("Dev server started and running at http://localhost:8080")
 		log.Fatal(http.ListenAndServe("localhost:8080", nil))
-	} else {
-		fmt.Println("Server started and running")
-		log.Fatal(http.ListenAndServe("0.0.0.0:"+os.Getenv("PORT"), nil))
 	}
+	fmt.Println("Server started and running")
+	log.Fatal(http.ListenAndServe("0.0.0.0:"+os.Getenv("PORT"), nil))
 }
 
 // handleHome is the handler for the home route ("/")
-func handleHome(w http.ResponseWriter, r *http.Request) {
+func (s *service) handleHome(w http.ResponseWriter, r *http.Request) {
+	// test DB query
+	dbUsers, err := s.db.ListUsers(r.Context())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(dbUsers)
+
 	tmpl := template.Must(template.ParseFiles("templates/index.html", "templates/fragments.html"))
-	err := tmpl.Execute(w, nil)
+	err = tmpl.Execute(w, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
--- go.mod
+++ go.mod
@@ -2,4 +2,15 @@ module github.com/wipdev-tech/pmdb
 
 go 1.21.6
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/tursodatabase/libsql-client-go v0.0.0-20240220085343-4ae0eb9d0898
+)
+
+require (
+	github.com/antlr/antlr4/runtime/Go/antlr/v4 v4.0.0-20230512164433-5d1fd1a340c9 // indirect
+	github.com/klauspost/compress v1.15.15 // indirect
+	github.com/libsql/sqlite-antlr4-parser v0.0.0-20230802215326-5cb5bb604475 // indirect
+	golang.org/x/exp v0.0.0-20220722155223-a9213eeb770e // indirect
+	nhooyr.io/websocket v1.8.7 // indirect
+)

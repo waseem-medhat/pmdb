ee99bb39f70ef0b3f317cae4ec1aaf1c43898e82
wipdev-tech <wipdev.tech@gmail.com>
2024-02-20 16:53:47 +0200

    Air config + initial server + templates

--- cmd/pmdb/main.go
+++ cmd/pmdb/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"text/template"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	fmt.Println("PMDb Server Let's Go! ")
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.HandleFunc("/", handleHome)
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if os.Getenv("ENV") == "dev" {
+		fmt.Println("Dev server started and running at http://localhost:8080")
+		log.Fatal(http.ListenAndServe("localhost:8080", nil))
+	} else {
+		fmt.Println("Server started and running")
+		log.Fatal(http.ListenAndServe("0.0.0.0:"+os.Getenv("PORT"), nil))
+	}
+}
+
+// handleHome is the handler for the home route ("/")
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/index.html", "templates/fragments.html"))
+	err := tmpl.Execute(w, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/wipdev-tech/pmdb
 
 go 1.21.6
+
+require github.com/joho/godotenv v1.5.1

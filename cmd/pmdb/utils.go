5ef57c4ee8b3f43cbc8426a86c775aff61a727f7
wipdev-tech <wipdev.tech@gmail.com>
2024-02-20 21:23:16 +0200

    Skip erroring out with godotenv on prod

--- cmd/pmdb/utils.go
+++ cmd/pmdb/utils.go
@@ -12,6 +12,13 @@ func getDBEnv() (string, string, error) {
 	dbToken := ""
 	err := godotenv.Load()
 
+	// In the Render prod deployment there is no .env by design
+	// So will error out locally but skip for prod
+	if os.Getenv("ENV") == "dev" && os.IsNotExist(err) {
+		return dbURL, dbToken, err
+	}
+	err = nil
+
 	dbURL = os.Getenv("DBURL")
 	if dbURL == "" {
 		err = fmt.Errorf("DBURL environment variable is not set")

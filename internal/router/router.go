dbf0994fa3b025cb1b79112081c2e76b828922da
wipdev-tech <wipdev.tech@gmail.com>
2024-03-14 14:04:01 +0200

    Refactored routes and handler names for clarity

--- internal/router/router.go
+++ internal/router/router.go
@@ -12,11 +12,11 @@ func New(s *service.Service) *http.ServeMux {
 	fs := http.FileServer(http.Dir("static"))
 	mux.Handle("GET /static/", http.StripPrefix("/static/", fs))
 	mux.HandleFunc("GET /{$}", s.HandleHome)
-	mux.HandleFunc("GET /register", s.HandleRegister)
+	mux.HandleFunc("GET /register", s.HandleRegisterGet)
 	mux.HandleFunc("GET /login", s.HandleLoginGet)
 
-	mux.HandleFunc("POST /create-user", s.HandleCreateUser)
-	mux.HandleFunc("POST /register/validate", s.HandleValidateRegisterForm)
+	mux.HandleFunc("POST /register", s.HandleRegisterPost)
+	mux.HandleFunc("POST /register/validate", s.HandleRegisterValidate)
 	mux.HandleFunc("POST /login", s.HandleLoginPost)
 
 	return mux
---
+++ internal/service/handle_register.go
@@ -0,0 +1,104 @@
+package service
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/wipdev-tech/pmdb/internal/database"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func (s *Service) HandleRegisterGet(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles(
+		"templates/register.html",
+		"templates/blocks/_top.html",
+		"templates/blocks/_bottom.html",
+	))
+	err := tmpl.Execute(w, struct{ ErrorMsgs []string }{})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (s *Service) HandleRegisterPost(w http.ResponseWriter, r *http.Request) {
+	displayName := r.PostFormValue("display-name")
+	userName := r.PostFormValue("user-name")
+	password := r.PostFormValue("password")
+	confirmPassword := r.PostFormValue("confirm-password")
+
+	if password != confirmPassword {
+		log.Fatal("passwords don't match")
+		return
+	}
+
+	hPassword, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+	if err != nil {
+		log.Fatal("couldn't hash password")
+		return
+	}
+
+	dbUser, err := s.DB.CreateUser(r.Context(), database.CreateUserParams{
+		ID:          uuid.NewString(),
+		UserName:    userName,
+		DisplayName: displayName,
+		Password:    string(hPassword),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tmplData := struct {
+		User database.User
+	}{
+		User: dbUser,
+	}
+
+	tmpl := template.Must(template.ParseFiles("templates/htmx/hx_register_success.html"))
+	err = tmpl.Execute(w, tmplData)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (s *Service) HandleRegisterValidate(w http.ResponseWriter, r *http.Request) {
+	errorMsgs := []string{}
+
+	inDisplayName := r.PostFormValue("display-name")
+	if inDisplayName == "" {
+		errorMsgs = append(errorMsgs, "Full name shouldn't be empty.")
+	}
+
+	dbUsers, err := s.DB.ListUsers(r.Context())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	inUserName := r.PostFormValue("user-name")
+	if inUserName == "" {
+		errorMsgs = append(errorMsgs, "User name shouldn't be empty.")
+	}
+
+	inPassword := r.PostFormValue("password")
+	if len(inPassword) < 8 {
+		errorMsgs = append(errorMsgs, "Password must be at least 8 characters long.")
+	}
+
+	inConfirmPassword := r.PostFormValue("confirm-password")
+	if inConfirmPassword != inPassword {
+		errorMsgs = append(errorMsgs, "Passwords should match.")
+	}
+
+	for _, u := range dbUsers {
+		if u.UserName == inUserName {
+			errorMsgs = append(errorMsgs, "User name already exists.")
+		}
+	}
+
+	tmpl := template.Must(template.ParseFiles("templates/register.html"))
+	err = tmpl.ExecuteTemplate(w, "check", struct{ ErrorMsgs []string }{ErrorMsgs: errorMsgs})
+	if err != nil {
+		log.Fatal(err)
+	}
+}

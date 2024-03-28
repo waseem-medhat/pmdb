eba96a31a876958cecb8697940f0c9dca5b38827
wipdev-tech <wipdev.tech@gmail.com>
2024-03-28 23:54:40 +0200

    Fixed bug that drops query params in requested URL

--- internal/service/handle_reviews.go
+++ internal/service/handle_reviews.go
@@ -11,19 +11,22 @@ import (
 func (s *Service) HandleReviewsNewGet(w http.ResponseWriter, r *http.Request) {
 	_, err := s.authJWTCookie(r)
 	if err != nil {
-		cookie := createCookie("pmdb-requested-url", r.URL.Path, "/login", 3600)
+		cookie := createCookie("pmdb-requested-url", r.URL.String(), "/login", 3600)
 		http.SetCookie(w, cookie)
 		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
 	}
 
 	movieId := r.URL.Query().Get("movieId")
 	if movieId == "" {
 		http.Redirect(w, r, "/", http.StatusPermanentRedirect)
+		return
 	}
 
 	movieDetails, err := tmdbapi.GetMovieDetails(movieId)
 	if err != nil {
 		http.Redirect(w, r, "/", http.StatusPermanentRedirect)
+		return
 	}
 
 	err = templs.NewReview(templs.NewReviewData{Movie: movieDetails}).Render(r.Context(), w)
